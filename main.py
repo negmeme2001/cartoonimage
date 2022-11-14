5c95b0d7f4e4d44df2bc2a7394debc41e3dc665d
Mohamed Ahmed Negm <83051202+negmeme2001@users.noreply.github.com>
2022-11-14 16:48:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# import the necessary packages
+import cv2
+import imutils
+
+#import the required libraries
+#import cv2
+import easygui 
+import imageio 
+import sys
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
